7805f1a81467f018db3511e8b1a6b421d56b5270
Lobazok <114778659+Lobazok@users.noreply.github.com>
2023-01-22 13:34:52 -0300

    Delete example.js

--- example.js
+++
@@ -1,6 +0,0 @@
-const bidan = require('./bidan')
-const { relu } = require("./Activationfunctions")
-
-var lian = new bidan.Neuralnetwork()
-lian.config(5, relu, [4, 3, 6, 4, 2], relu, 1, relu)
-lian.info()
