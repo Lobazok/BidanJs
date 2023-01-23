922f7b6f180ac98554ceabc8014ac148d4141094
Moises Meza(Lobazok) <cuenta.tv.lobo7@gmail.com>
2023-01-23 17:33:03 -0300

    se fusiona neurons y bidan

--- modules/bidan.js
+++ modules/bidan.js
@@ -1,7 +1,6 @@
 const colors = require("colors/safe")
 const fs = require("fs")
 const { funcions } = require("./func/Activationfunctions")
-const { perceptron } = require("./neurons")
 
 colors.setTheme({
     error: ["red", "italic", "bold"],
@@ -19,6 +18,56 @@ function logError(error) {
     console.log(colors.error(error))
 }
 
+class perceptron {
+    constructor(name, Activationfunction) {
+        this.name = name
+        this.Activationfunction = Activationfunction
+        this.Input = []
+        this.ActivationInput
+        this.pesos = []
+        this.sesgo = 0
+        this.Output = []
+    }
+
+    addInput = (num) => {
+        this.Input.push(num)
+    }
+    cal = () => {
+        if (typeof this.Input == "object") {
+            let r = 0;
+            for (let i = 0; i < this.Input.length; i++) {
+                if (this.pesos.length != 0) {
+                    r += this.Input[i] * this.pesos[i];
+                } else r += this.Input[i]
+            }
+            r += this.sesgo;
+            return r;
+        } else logError("Bidan error 004: la neurona: " + this.name + " no risivio un array de numeros como input");
+    }
+    activation = () => {
+        if (typeof this.Input == "object") {
+            if (typeof this.Activationfunction == "function") {
+                if (this.ActivationInput === this.Input.length) {
+                    let result = this.Activationfunction(this.cal());
+                    for (let index = 0; index < this.Output.length; index++) {
+                        this.Output[index].addInput(result);
+                        this.Output[index].activation()
+                    }
+                }
+            } else {
+                logError("Bidan error 002: la funcion de activacion de la nueronas:" + this.name + " no es una funcion");
+            }
+
+        } else logError("Bidan error 004: la neurona: " + this.name + " no risivio un array de numeros como input");
+    }
+
+    info = () => {
+        console.log("Neuron: " + this.name);
+        console.log(" Activation function: " + this.Activationfunction.name);
+        console.log(" result bruto: " + this.cal());
+        console.log(" result: " + this.Activationfunction(this.cal()));
+    }
+}
 
 class Neuralnetwork {
     constructor() {
--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const { relu, sigmoid } = require("../modules/func/Activationfunctions.js")
 
 var lian = new bidan.Neuralnetwork()
 lian.LayerInputConfig(1, relu)
-lian.LayersConfig([7, 2,1], [relu, relu, sigmoid])
+lian.LayersConfig([7, 2, 1], [relu, relu, sigmoid])
 lian.LayerOutputConfig(1, sigmoid)
 
 lian.saveCofig("lianDataConfig")
@@ -14,3 +14,11 @@ lian.LayerInput[0].addInput(1)
 lian.LayerInput[0].activation()
 
 lian.LayerOutput[0].info()
+
+const fun = (expectedOutput, obtainedOutput) => {
+    let error = 0
+    for (let i = 0; i < expectedOutput.length; i++) {
+         error += (expectedOutput[i] - obtainedOutput[i]) ** 2
+    }
+    return error / expectedOutput.length
+ }
